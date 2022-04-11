df3766f4842ece5dd2e39cec047a8cc480fee629
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-11 17:22:49 +0200

    esercizio base svolto

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+   
+    <Title></Title>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    Title
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>La mia prima app con Vue CLI!</h2>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+
+<style scoped>
+h2{
+    color: red;
+}
+</style>
